45775d1c3fc1ce1e2620e1688cdcb312ad5e9c90
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-25 20:19:38 +0300

    Задача 23

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int N = UserCoord("Введите число N: ");
+MethodCube(N);
+
+//---------------------------------------Методы---------------------
+//-------------Таблица кубов
+void MethodCube(int Number)
+{
+    if(Number<0) 
+    {
+        //Number=Number*(-1);
+        for(int i = -1; i>= Number; i--)
+        {
+        Console.WriteLine($"{i}^3 = {i*i*i}");
+        }
+    }
+    
+    for(int i = 1; i<= Number; i++)
+    {
+    Console.WriteLine($"{i}^3 = {i*i*i}");
+    }
+}
+
+//----------------Ввод данных
+static int UserCoord(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
