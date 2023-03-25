1f80f24342a4199ec4e763a2f39b7d7bb74f851a
Bazhaeva Madina <bazhaevaweb@gmail.com>
2023-03-25 20:11:06 +0300

    Задача 19

--- task_19/Program.cs
+++ task_19/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberN = UserNumber("Введите пятизначное число: ");
+MethodPalindrome(numberN);
+
+//--------------------------------Методы----------------------------------
+//----------------------------Палиндром
+void MethodPalindrome(int Number)
+{
+    int i = 0;
+string resDescription = String.Empty;     
+int countN = (int)Math.Log10(numberN) + 1;
+
+while(i<countN/2)
+{
+    if((numberN/(int)Math.Pow(10, i))%10 == (numberN/(int)Math.Pow(10, countN-1-i))%10)
+    {
+        i=i+1;
+        resDescription = "Да";
+    }
+    else
+    {
+        resDescription = "Нет";
+        break;
+    }
+}
+Console.Write($"{numberN} -> {resDescription}");
+}
+
+
+
+//----------------Ввод данных
+static int UserNumber(string messageToUser)
+{
+    Console.Write(messageToUser);
+    try
+    {
+        return int.Parse(Console.ReadLine() ?? "");
+    }
+    catch (Exception exc)
+    {
+        Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+        return 0;
+    }
+}
